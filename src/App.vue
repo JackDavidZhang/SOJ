f4c3a8335636ca6c7052b3c8aca8806e0a62c68c
ztsubaki <2949125232@qq.com>
2024-12-10 16:24:34 +0800

    Add soj-user-self.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import SojMenu from "./components/soj-menu.vue";
 import SojCarousel from "./components/soj-carousel.vue";
+import SojUserSelf from "./components/soj-user-self.vue";
 </script>
 
 <template>
@@ -8,7 +9,12 @@ import SojCarousel from "./components/soj-carousel.vue";
     <d-header>
       <soj-menu></soj-menu></d-header>
     <d-content>
-      <soj-carousel></soj-carousel>
+      <d-row>
+        <d-col span="20" class="dcol">
+          <soj-carousel></soj-carousel></d-col>
+        <d-col span="4" class="dcol">
+          <soj-user-self></soj-user-self></d-col>
+      </d-row>
     </d-content>
   </d-layout>
 </template>
@@ -16,5 +22,8 @@ import SojCarousel from "./components/soj-carousel.vue";
 <style scoped>
 #app{
 width: 100%;
+}
+.dcol{
+  margin: 5px;
 }
 </style>
---
+++ src/components/soj-user-self.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+</script>
+
+<template>
+<d-card shadow="never" class="self-card">
+  <template #avatar>
+    <d-avatar name="Dev" width="80" height="80">
+    </d-avatar>
+  </template>
+  <template #title>
+    Dev Account
+  </template>
+  <template #subtitle>
+    <d-icon name="company-member"></d-icon><span>Administrator</span>
+  </template>
+</d-card>
+</template>
+
+<style scoped>
+.self-card{
+  height: 100%;
+}
+</style>
